fef08bfa6c46810f5ae2ef6aeba7cf9b82d6daf7
Rayen-Dhafer <rayendh7@gmail.com>
2024-04-15 19:28:45 +0200

    save arezzo files

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-//express : création des apis 
-//mongoose : biblio base de donnees mongodb
-//cors : autorisation de la consomation des apis
-
-//nodemon
-
-// node-mailer : envoie des email
-//connect-multiparty : upload des images
-
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -14,42 +5,53 @@ const cors = require('cors')
 const multiparty = require('connect-multiparty')
 const multipartyMiddleware = multiparty({uploadDir: './uploads'})
 
-// config du serveur
+
+const userRoutes = require("./routes/user.routes")
+const adminRoutes = require("./routes/admin.routes")
+
+
+
 var serveur = express()
 serveur.use(cors())
-serveur.use(express.json()) // lire le contenu du formaulaire
+serveur.use(express.json()) 
+serveur.use(express.static('./')) 
 
-serveur.use(express.static('./')) // lire les fichier (images)
-// connexion à la base de données
-mongoose.connect("mongodb://localhost:27017/mygcord_db", () => {
-    console.log('mongo connected')
+
+//nodemon
+
+mongoose.connect("mongodb://localhost:27017/Digital_City_Db", () => {
+
+        console.log("database connected successfully ",mongoose.connection.port)
+
 })
-
-
-const userCtrl = require('./utilisateurs.controller')
-serveur.post('/inscription', userCtrl.inscription)
-serveur.post('/login' , userCtrl.login)
-serveur.post('/emailexist' , userCtrl.emailexist)
-serveur.post('/validate' , userCtrl.validate)
-serveur.post('/savepassword' , userCtrl.savepassword)
-serveur.post('/sendCode' , userCtrl.sendCode)
-serveur.post('/Upcouverture' , userCtrl.Upcouverture)
-serveur.post('/getuser' , userCtrl.getuser)
-serveur.get('/getall' , userCtrl.getall)
-
-
-
-
 
 
 
 serveur.post('/upload' , multipartyMiddleware , (requete , reponse)=>{
-    var file = requete.files
 
-    console.log(file)
-    reponse.send(file)
+    var path =''
+    if(requete.files.files){
+        path= requete.files.files.path
+    }
+    reponse.send({path : path})
+    
 })
+
+
+
+
+serveur.use("/user",userRoutes)
+serveur.use("/admin",adminRoutes)
+
+
+
+
+
+
+
 
 serveur.listen(3000, () => {
     console.log('server runnn')
 })
+
+
